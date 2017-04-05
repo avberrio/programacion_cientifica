4c1061c06639a74563741b8ac11db13fdf01608d
avberrio <avberrio@github.com>
2017-04-04 23:36:56 -0300

    Actividad Clase 1 Lista

---
+++ Actividades/Actividad 1/_temp.py
@@ -0,0 +1,12 @@
+a = float(input("Ingrese a: "))
+b = float(input("Ingrese b: "))
+c = float(input("Ingrese c: "))
+
+if a>(b+c) or b>(a+c) or c>(a+b):
+    print(a = float(input("Ingrese a: ")))
+elif a==b and b==c:
+    print(a = float(input("Ingrese a: ")))
+elif a==b or b==c or a==c:
+    print(a = float(input("Ingrese a: ")))
+else:
+    print(a = float(input("Ingrese a: ")))
